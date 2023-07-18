95509b39df70ed44965440320478939facf7fe0f
kamilord003 <kamilord003.com>
2023-07-18 18:39:48 -0500

    Primer ejemplo: Informacion Mercadolibre

--- WebNoticiesScraping.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import pandas as pd
-import time
-
-
-driver =  webdriver.Chrome("./chromedriver.exe")
-driver.get("https://www.mercadolibre.com.co/")
-driver.close()
